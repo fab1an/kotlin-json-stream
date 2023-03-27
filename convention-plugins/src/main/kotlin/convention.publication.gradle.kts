cd21482ff76915e1d8d0f4a7010a67884561ae1d
Fabian Zeindl <fabian.zeindl@gmail.com>
2023-03-27 10:34:54 +0200

    [build] added maven configuration

---
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -0,0 +1,84 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.`maven-publish`
+import org.gradle.kotlin.dsl.signing
+import java.util.*
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.secretKeyRingFile"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply {
+            load(it)
+        }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(javadocJar.get())
+
+        pom {
+            name.set("Kotlin Json Stream")
+            description.set("Kotlin Multiplatform json stream library")
+            url.set("https://github.com/fab1an/kotlin-json-stream")
+
+            licenses {
+                license {
+                    name.set("Apache-2.0")
+                    url.set("https://opensource.org/license/apache-2-0/")
+                }
+            }
+            developers {
+                developer {
+                    id.set("https://github.com/fab1an/")
+                    name.set("Fabian Zeindl")
+                    email.set("fabian@fabianzeindl.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/fab1an/kotlin-json-stream")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
