89082306edbd82537ea744025219491a75f152b8
Fabian Zeindl <fabian.zeindl@gmail.com>
2023-05-30 07:00:25 +0200

    [code] added documentation and fixed dokka links to okio

--- src/commonMain/kotlin/com/fab1an/kotlinjsonstream/Extensions.kt
+++ src/commonMain/kotlin/com/fab1an/kotlinjsonstream/Extensions.kt
@@ -1,11 +1,26 @@
 package com.fab1an.kotlinjsonstream
 
+/**
+ * Writes a [list] of [E] to this [JsonWriter], using the supplied [writerFn].
+ *
+ * @param E the type of the objects
+ * @param list the list of objects
+ * @param writerFn the writer function that is called using each item of the [list] as input
+ */
 inline fun <E : Any> JsonWriter.value(list: List<E>, writerFn: JsonWriter.(E) -> Unit) {
     beginArray()
     list.forEach { writerFn(it) }
     endArray()
 }
 
+/**
+ * Reads a [list] of [E] from this [JsonReader]. This function assumes it is called at the start
+ * of a JSON-array and reads each element using the supplied [readerFn].
+ *
+ * @param E the type of the objects
+ * @param readerFn the reader function that is called using each item of the array as input
+ * @return list of E
+ */
 inline fun <E : Any> JsonReader.nextList(readerFn: JsonReader.() -> E): List<E> {
     val list = mutableListOf<E>()
 
