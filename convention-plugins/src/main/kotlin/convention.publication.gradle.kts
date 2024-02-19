756edc6b2607c158099fbaacce9826b5f2d69868
Fabian Zeindl <fabian.zeindl@gmail.com>
2024-02-19 06:58:19 +0100

    [code] updated gradle to 8.4, kotlin to 1.9.22 and added linuxArm64 target

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -1,8 +1,5 @@
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.kotlin.dsl.`maven-publish`
-import org.gradle.kotlin.dsl.signing
-import java.util.*
+
+import java.util.Properties
 
 plugins {
     `maven-publish`
